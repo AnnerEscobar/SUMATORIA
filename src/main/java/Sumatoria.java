a4cf13133a65679af2f1acae784f429b73a99a8f
ANNER RONALDO ESCOBAR CRUZ <aescobarc13@miumg.edu.gt>
2022-02-18 19:40:10 -0600

    Primera subida

---
+++ src/main/java/Sumatoria.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Anner
+ */
+public class Sumatoria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] numeros = new int[]{1,2,3,4,5,6,7,8,9};
+        int contador =0;
+        for(int i=0; i<numeros.length; i++){
+            if(numeros[i]%2==0){
+                contador ++;
+            }
+        }
+        System.out.println("lA SUMATORIA ES " + contador);
+        
+        
+    }
+    
+}
